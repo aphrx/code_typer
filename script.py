d947b42ac43aed6945927c306c54deedf045166d
aphrx <amalnnath20@yahoo.ca>
2021-01-10 00:29:11 -0500

    init commit

---
+++ script.py
@@ -0,0 +1,31 @@
+import pyautogui
+import sys
+import time
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Incorrect number of parameters. Please execute with file name as parameter")
+    else:
+        filename = sys.argv[1]
+        data = open(filename, "r").read()
+        time.sleep(2)
+        tab_counter = 0
+        for d in data:
+            if d == "\n":
+                tab_counter = 0
+                pyautogui.press(d)
+                time.sleep(1)
+            elif d == " ":
+                tab_counter += 1
+
+            else:
+                if tab_counter % 4 == 0:
+                    for i in range(int(tab_counter/4)):
+                        pyautogui.press("tab")
+                else:
+                    for i in range(tab_counter):
+                        pyautogui.press(" ")
+                tab_counter = 0
+                pyautogui.press(d)
+
+
